7f5c1e45796c210540ffe14fb9d93f53ae33e587
Imad Jhabli <imadjhabli@gmail.com>
2024-02-03 20:22:32 +0100

    fix attempt

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,41 +1,31 @@
 #!/usr/bin/python3
 """first endpoint (route) will be to return the status of my API"""
-import os
+from os import getenv
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
-@app.errorhandler(404)
-def error_404(exception):
-    return {"error": "Not found"}, 404
-
-
-@app.errorhandler(400)
-def error_400(exception):
-    message = exception.description
-    return message, 400
-
-
 @app.teardown_appcontext
-def close_db(ctx):
+def teardown_db(exception=None):
+    """closes storage"""
     storage.close()
 
 
-if os.getenv("HBNB_API_HOST"):
-    host = os.getenv("HBNB_API_HOST")
+if getenv("HBNB_API_HOST"):
+    host = getenv("HBNB_API_HOST")
 else:
     host = "0.0.0.0"
 
-if os.getenv("HBNB_API_PORT"):
-    port = int(os.getenv("HBNB_API_PORT"))
+if getenv("HBNB_API_PORT"):
+    port = int(getenv("HBNB_API_PORT"))
 else:
     port = 5000
 
 
 if __name__ == "__main__":
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
