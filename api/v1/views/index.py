25505f5d80802c7d19add858de6730ce61f1ea7d
Mehdisaber332 <133115021+Mehdisaber332@users.noreply.github.com>
2024-02-04 09:13:11 +0100

    task 4 done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
-"""creating route on objects"""
-
-from models import storage
+"""
+contains endpoints(routes)
+"""
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 
-@app_views.route("/status", strict_slashes=False, methods=["GET"])
+@app_views.route("/status", strict_slashes=False)
 def status():
-    return {
-        "status": "OK",
-    }
+    """
+    returns a JSON: "status": "OK"
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """ retrieves the number of each objects by type """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
